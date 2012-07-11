4abcd65d8eca43c8a970fc17dfaa44912bf3cc2c
Jess Chadwick <jesschadwick@gmail.com>
2012-07-11 17:02:02 -0400

    Adding Server project

---
+++ Server/Controllers/NewsFeedController.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using RealTimeWeb.Models;
+
+namespace RealTimeWeb.Controllers
+{
+    // http://msdn.microsoft.com/en-us/library/ee728598.aspx
+    public class NewsFeedController : AsyncController
+    {
+        public ActionResult TopStories()
+        {
+            return GetTopStoriesResult();
+        }
+
+        public async Task<ActionResult> TopStoriesWithAsync()
+        {
+            var service = new NewsService();
+            await service.GetTopStoriesAsync();
+        }
+
+        private ActionResult GetTopStoriesResult()
+        {
+            var service = new NewsService();
+            var topStories = await service.GetTopStoriesAsync();
+
+            var format = Request["format"];
+
+            if (Request.IsAjaxRequest() || format == "partial")
+                return PartialView("Stories", topStories);
+
+            if (Request.ContentType.StartsWith("application/json")
+                || format == "json")
+                return Json(topStories, JsonRequestBehavior.AllowGet);
+
+            ViewBag.Title = "Top Stories";
+            return View("Stories", topStories);
+        }
+    }
+}
