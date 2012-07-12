ba3958fff9fc2d45c90f6f86c59afcd05885db0c
Jess Chadwick <jesschadwick@gmail.com>
2012-07-12 16:58:34 -0400

    Showing server-side code

--- Website/Controllers/NewsFeedController.cs
+++ Website/Controllers/NewsFeedController.cs
@@ -1,35 +1,51 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using RealTimeWeb.Models;
 
 namespace RealTimeWeb.Controllers
 {
-    public class NewsFeedController : Controller
+    public class NewsFeedController : AsyncController
     {
+/*
+        // Non-Async Action
         public ActionResult Index(int last = 0, int? delay = null)
         {
             var topStories = new NewsService().GetTopStories(last, delay);
             return PartialView("Stories", topStories);
         }
+*/
 
-        public ActionResult LongPoll(int last = 0, int? delay = null)
+        // Async Action
+        public Task<ActionResult> Index(int last = 0, int? delay = null)
         {
-            var service = new NewsService();
-            IEnumerable<NewsStory> topStories;
-
-            do
-            {
-                topStories = service.GetTopStories(last, delay);
-            } while (!topStories.Any());
-
-            return PartialView("Stories", topStories);
+            return Task.Factory.StartNew<ActionResult>(() => {
+                var topStories = new NewsService().GetTopStories(last, delay);
+                return PartialView("Stories", topStories);
+            });
         }
+
+        public Task<ActionResult> LongPoll(int last = 0, int? delay = null)
+        {
+            return Task.Factory.StartNew<ActionResult>(() => {
+                var service = new NewsService();
+                IEnumerable<NewsStory> topStories;
+
+                do
+                {
+                    topStories = service.GetTopStories(last, delay);
+                } while (!topStories.Any());
+
+                return PartialView("Stories", topStories);
+            });
+        }
+
 
 
         public ActionResult Add(NewsStory story = null)
         {
-            if(story == null || string.IsNullOrEmpty(story.Title))
+            if (story == null || string.IsNullOrEmpty(story.Title))
                 story = new NewsService().AddStory();
             else
                 new NewsService().AddStory(story);
---
+++ Website/Extensions/HtmlExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+public static class HtmlExtensions
+{
+
+    public static IHtmlString RenderSourceCode(this HtmlHelper helper, string relativePath)
+    {
+        var serverPath = helper.ViewContext.HttpContext.Server.MapPath(relativePath);
+
+        if (!File.Exists(serverPath))
+            return new HtmlString(string.Format("<!-- File not found: {0} -->", serverPath));
+
+        return new HtmlString(string.Format("<label>{0}</label><pre>{1}</pre>", relativePath, File.ReadAllText(serverPath)));
+    }
+
+}
