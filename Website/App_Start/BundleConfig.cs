46175494f03b7bf2a1beb6dcace6614a7d3262e9
Jess Chadwick <jesschadwick@gmail.com>
2012-07-12 13:50:46 -0400

    Importing layout

--- Website/App_Start/BundleConfig.cs
+++ Website/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace RealTimeWeb
 {
@@ -17,7 +16,7 @@ namespace RealTimeWeb
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/styles.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
