7508c98f50519e371b31679a1d4f55e8167c8a12
Jess Chadwick <jesschadwick@gmail.com>
2012-07-12 17:43:55 -0400

    Server-sent events

---
+++ Website/Handlers/NewsFeedServerSentEvents.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="NewsFeedServerSentEvents.ashx.cs" Class="RealTimeWeb.Handlers.NewsFeedLongServerSentEvents" %>
---
+++ Website/Handlers/NewsFeedServerSentEvents.ashx.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Web;
+using Newtonsoft.Json;
+using RealTimeWeb.Models;
+
+namespace RealTimeWeb.Handlers
+{
+    public class NewsFeedLongServerSentEvents : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            int last;
+            int.TryParse(context.Request["last"], out last);
+
+            context.Response.Clear();
+            context.Response.ContentType = "text/event-stream";
+
+            var service = new NewsService();
+
+            // Loop until the request is aborted!
+            while(true)
+            {
+                var stories = service.GetTopStories(last);
+
+                if(stories.Any())
+                {
+                    context.Response.Write(JsonConvert.SerializeObject(stories));
+                    context.Response.Flush();
+                }
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
